2d3e5a81211db84722a42712dc626fc6d367bd72
user <hag.hispin12@gmail.com>
2026-04-14 23:00:41 +0300

    make login screen and save user data in ram(for now)

--- app/src/main/java/com/example/danielproject_chess/LoginFragment.java
+++ app/src/main/java/com/example/danielproject_chess/LoginFragment.java
@@ -7,6 +7,8 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginFragment extends Fragment {
 
@@ -20,11 +22,16 @@ public class LoginFragment extends Fragment {
 
     }
 
+    Button loginBtn;
+    EditText username;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View viw = inflater.inflate(R.layout.fragment_login, container, false);
+        loginBtn = viw.findViewById(R.id.login_btn);
+        username = viw.findViewById(R.id.username);
 
         return viw;
     }
--- app/src/main/java/com/example/danielproject_chess/MainActivity.java
+++ app/src/main/java/com/example/danielproject_chess/MainActivity.java
@@ -1,14 +1,21 @@
 package com.example.danielproject_chess;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.LinearLayout;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
     private LinearLayout mainLayout;
     private Board b;
+    private ActivityResultLauncher<Intent> loginListener;
+
 
 
     @Override
@@ -22,9 +29,15 @@ public class MainActivity extends AppCompatActivity {
     }
     public void init(){
         mainLayout = findViewById(R.id.board);
+
     }
     public void launchLogin(){
-
+        loginListener = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), new ActivityResultCallback<ActivityResult>() {
+            @Override
+            public void onActivityResult(ActivityResult result) {
+                //todo: add users
+            }
+        });
     }
     public void startBoard() {
         b = new Board(this, mainLayout);
